720254d873d771f1b0e7c2589d61e1a65c0dfc1c
dup06087 <83522094+dup06087@users.noreply.github.com>
2022-09-26 22:01:52 +0900

    Update execute_file.py

--- execute_file.py
+++ execute_file.py
@@ -1,16 +1,18 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 import Get_Equipment_status
-import prac_scroll
+# import prac_scroll
+import prac_childwindow
 from PyQt5.QtCore import *
 from to_class.get_cam_num import camera_num
 
 ### Qt 실행
-app = QtWidgets.QApplication(sys.argv)
-MainWindow = QtWidgets.QMainWindow()
-ui = prac_scroll.Ui_MainWindow()
-ui.setupUi(MainWindow)
-MainWindow.show()
+# app = QtWidgets.QApplication(sys.argv)
+# MainWindow = QtWidgets.QMainWindow()
+# ui = prac_scroll.Ui_MainWindow()
+# ui = prac_childwindow.Ui_MainWindow()
+# ui.setupUi(MainWindow)
+# MainWindow.show()
 
 ### 장비가져오기 쓰레드 실행
 thread_get_data = QThread()
@@ -29,14 +31,14 @@ class Worker_get_data(QObject):
                 for i in range(camera_num):
                     print("temperature getting")
                     # test =
-                    ui.status_label[i].setText(Get_Equipment_status.values[i])
+                    prac_childwindow.ui.status_label[i].setText(Get_Equipment_status.values[i])
                     QtWidgets.QApplication.processEvents()  ### 중요
 
-                    print("value[i] : ", Get_Equipment_status.values[i], "\n values end")
+                    # print("value[i] : ", Get_Equipment_status.values[i], "\n values end")
                     if (int(Get_Equipment_status.temp_outputs[i]) >= 25) and (int(Get_Equipment_status.volt1_outputs[i]) >= 25) and (int(Get_Equipment_status.volt2_outputs[i]) >= 25) and (int(Get_Equipment_status.volt3_outputs[i]) >= 25) and (int(Get_Equipment_status.current_outputs[i]) >= 25):
-                        ui.lbl_prob_chk[i].setStyleSheet("background-color: rgb(255, 0, 0);")
+                        prac_childwindow.ui.lbl_prob_chk[i].setStyleSheet("background-color: rgb(255, 0, 0);")
                     else:
-                        ui.lbl_prob_chk[i].setStyleSheet("background-color: rgb(255, 255, 255);")
+                        prac_childwindow.ui.lbl_prob_chk[i].setStyleSheet("background-color: rgb(255, 255, 255);")
             except:
                 print("UI loading...")
 
@@ -44,4 +46,4 @@ worker_get_data = Worker_get_data()
 worker_get_data.moveToThread(thread_get_data)
 thread_get_data.started.connect(worker_get_data.get_temperature())
 
-sys.exit(app.exec_())
+# sys.exit(prac_childwindow.app.exec_())
